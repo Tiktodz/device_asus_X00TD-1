6db934d27a76b33336f3505115b3bc8af07d3633
Meisie Widuri <kuplemarkeple@gmail.com>
2024-04-25 20:03:18 +0700

    X00TD: Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+rm -rf packages/apps/ParanoidSense
+git clone --depth=1 https://git.mainlining.org/EvolutionX/packages_apps_ParanoidSense -b uqpr2 packages/apps/ParanoidSense
+
+export TZ=Asia/Jakarta
