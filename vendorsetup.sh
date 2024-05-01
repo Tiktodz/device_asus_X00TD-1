557b5e37f61817fe4fde204cfb463b2595e4705b
Meisie Widuri <kuplemarkeple@gmail.com>
2024-05-01 10:57:35 +0700

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-rm -rf packages/apps/ParanoidSense
-git clone --depth=1 https://git.mainlining.org/EvolutionX/packages_apps_ParanoidSense -b uqpr2 packages/apps/ParanoidSense
-
-export TZ=Asia/Jakarta
